8603015dcb3909c7a333c1a2c6c9d612f195d699
El Wafi Cherkaoui <elwafi.cherkaoui@gmail.com>
2025-08-20 21:12:05 +0100

    Fix: forwarding issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,7 @@ export default async function middleware(request: NextRequest) {
     const isProtected = protectedRoutes.some((route)=>
         pathname.startsWith(route)
     )
-    const sessionCookie = request.cookies.get("__Secure-next-auth.session-token") ||
-        request.cookies.get("next-auth.session-token"); 
+    const sessionCookie = request.cookies.get("authjs.session-token") 
     
     if(isProtected && !sessionCookie){
         return NextResponse.redirect(new URL("/api/auth/signin", request.url))
